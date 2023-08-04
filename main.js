c839665063534ec0f7ebfa06b6d52141bfe16fe1
Allan Heremi <allanheremidev@gmail.com>
2023-08-04 13:07:59 +0200

    Feat: Meshes added + button to regenerate mesh Fix: Modularize

--- main.js
+++ main.js
@@ -2,16 +2,40 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.132.2/build/three.m
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { gsap } from 'gsap';
 
-// Function to generate a random color
 function getRandomColor() {
   return Math.floor(Math.random() * 16777215).toString(16);
 }
 
-// Function to generate a random mesh
 function generateRandomMesh() {
   const randomColor = getRandomColor();
 
-  const geometry = new THREE.SphereGeometry(3, 64, 64);
+  let meshArr = [
+    new THREE.SphereGeometry(3, 64, 64),
+    new THREE.BoxGeometry(5, 5, 5),
+    new THREE.ConeGeometry(3, 5, 16),
+    new THREE.CylinderGeometry(1, 1, 5, 6),
+    new THREE.TorusGeometry(2, 1, 36, 100),
+    new THREE.TorusKnotGeometry(2, 0.4, 16, 16),
+    
+    new THREE.SphereGeometry(2, 62, 62),
+    new THREE.BoxGeometry(4, 4, 4),
+    new THREE.ConeGeometry(2, 4, 18),
+    new THREE.CylinderGeometry(1, 1, 5, 4),
+    new THREE.TorusGeometry(2, 1, 28, 80),
+    new THREE.TorusKnotGeometry(2, 0.4, 16, 24),
+    
+    new THREE.SphereGeometry(3, 64, 64),
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.ConeGeometry(2, 4, 11),
+    new THREE.TorusGeometry(2, 1, 12, 33),
+    new THREE.TorusKnotGeometry(2, 0.4, 12, 14),
+    
+ 
+    
+    
+  ];
+
+  const geometry = meshArr[Math.floor(Math.random() * meshArr.length)];
   const material = new THREE.MeshStandardMaterial({
     color: `#${randomColor}`,
     roughness: 0.69,
@@ -21,14 +45,28 @@ function generateRandomMesh() {
   return mesh;
 }
 
-// Set up the scene
 const scene = new THREE.Scene();
 const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 7, 40);
-light.intensity = 2;
+light.position.set(10, 0, -10);
+light.intensity = 1.25;
 scene.add(light);
 
-// Set up the camera
+const light1 = new THREE.PointLight(0xffffff, 1);
+light1.position.set(0, 10, 10);
+scene.add(light1);
+
+const light2 = new THREE.PointLight(0xffffff, 1);
+light2.position.set(10, 0, 10);
+scene.add(light2);
+
+const light3 = new THREE.PointLight(0xffffff, 1);
+light3.position.set(0, -10, 10);
+scene.add(light3);
+
+const light4 = new THREE.PointLight(0xffffff, 1);
+light4.position.set(-10, 0, 10);
+scene.add(light4);
+
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
@@ -36,9 +74,9 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.z = 18;
+
 scene.add(camera);
 
-// Set up the renderer
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -47,7 +85,6 @@ const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 
-// Set up the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.enablePan = false;
@@ -55,11 +92,16 @@ controls.enableZoom = false;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 5;
 
-// Call the function to generate a random mesh and add it to the scene
-const mesh = generateRandomMesh();
+function generateAndReplaceMesh() {
+  const newMesh = generateRandomMesh();
+  scene.remove(mesh);
+  mesh = newMesh;
+  scene.add(mesh);
+}
+
+let mesh = generateRandomMesh();
 scene.add(mesh);
 
-// Animate the scene
 const loop = () => {
   controls.update();
   renderer.render(scene, camera);
@@ -67,13 +109,11 @@ const loop = () => {
 };
 loop();
 
-// Set up GSAP animations
 const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo('nav', { y: '-100%' }, { y: '0%' });
 tl.fromTo('.title', { opacity: 0 }, { opacity: 1 });
 
-// Mouse interaction
 let mouseDown = false;
 let rgb = [];
 window.addEventListener('mousedown', () => (mouseDown = true));
@@ -93,3 +133,6 @@ window.addEventListener('mousemove', e => {
     });
   }
 });
+
+const titleButton = document.querySelector('.titleButton');
+titleButton.addEventListener('click', generateAndReplaceMesh);
