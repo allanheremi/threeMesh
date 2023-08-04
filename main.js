087f306474fb388d085668da12b32626d6e9a9f2
Allan Heremi <allanheremidev@gmail.com>
2023-08-04 12:10:07 +0200

    Fix: Navbar refactor

--- main.js
+++ main.js
@@ -2,50 +2,52 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.132.2/build/three.m
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { gsap } from 'gsap';
 
+// Function to generate a random color
+function getRandomColor() {
+  return Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// Function to generate a random mesh
+function generateRandomMesh() {
+  const randomColor = getRandomColor();
+
+  const geometry = new THREE.SphereGeometry(3, 64, 64);
+  const material = new THREE.MeshStandardMaterial({
+    color: `#${randomColor}`,
+    roughness: 0.69,
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
+}
+
+// Set up the scene
 const scene = new THREE.Scene();
-
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({
-  color: '#00ff83',
-  roughness: 0.69
-});
-
 const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(0, 10, 10);
-light.intensity = 1.25
+light.position.set(0, 7, 40);
+light.intensity = 2;
 scene.add(light);
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
+// Set up the camera
 const camera = new THREE.PerspectiveCamera(
   45,
-  sizes.width / sizes.height,
+  window.innerWidth / window.innerHeight,
   0.1,
   100
 );
 camera.position.z = 18;
 scene.add(camera);
 
+// Set up the renderer
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
-renderer.setPixelRatio(3);
 
-window.addEventListener('resize', () => {
-  (sizes.width = window.innerWidth), (sizes.height = window.innerHeight);
-
-  camera.updateProjectionMatrix();
-  camera.aspect = sizes.width / sizes.height;
-  renderer.setSize(sizes.width, sizes.height);
-});
-
+// Set up the controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.enablePan = false;
@@ -53,24 +55,29 @@ controls.enableZoom = false;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 5;
 
+// Call the function to generate a random mesh and add it to the scene
+const mesh = generateRandomMesh();
+scene.add(mesh);
+
+// Animate the scene
 const loop = () => {
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(loop);
 };
-
 loop();
 
+// Set up GSAP animations
 const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo('nav', { y: '-100%' }, { y: '0%' });
 tl.fromTo('.title', { opacity: 0 }, { opacity: 1 });
 
+// Mouse interaction
 let mouseDown = false;
 let rgb = [];
 window.addEventListener('mousedown', () => (mouseDown = true));
 window.addEventListener('mouseup', () => (mouseDown = false));
-
 window.addEventListener('mousemove', e => {
   if (mouseDown) {
     rgb = [
@@ -78,9 +85,7 @@ window.addEventListener('mousemove', e => {
       Math.round((e.pageY / sizes.height) * 255),
       150,
     ];
-    let newColor = new THREE.Color(`rgb(${rgb.join(',')})`);
-
-    
+    const newColor = new THREE.Color(`rgb(${rgb.join(',')})`);
     gsap.to(mesh.material.color, {
       r: newColor.r,
       g: newColor.g,
@@ -88,9 +93,3 @@ window.addEventListener('mousemove', e => {
     });
   }
 });
-
-
-document.querySelector('titleButton') = button
-button.addEventListener('click', () => {
-  geometry = new THREE.RingGeometry(3, 64, 64)
-})
